635e23fe3c0d4f11c00c669fdda32b7879fa1ad2
Tiltusha <elcoronabooking@gmail.com>
2023-05-21 10:08:20 +0300

    czcxыва

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -87,10 +87,15 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chatikdb',
+        'USER': 'postgres',
+        'PASSWORD': 'ElCorona',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
